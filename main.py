629224ed01c4e2c42e288e9ffe0faa08c67b3995
mnk0015 <122189075+mnk0015@users.noreply.github.com>
2025-11-06 20:05:05 +0000

    dp solution

---
+++ main.py
@@ -0,0 +1,8 @@
+def rob(nums):
+    dp = [0] * len(nums)
+    dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+    for i in range(2, len(nums)):
+        dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+    return dp[-1]
+
+print(rob([2,7,9,3,1]))  # fast, returns 12
